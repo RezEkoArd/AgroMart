a7296662b8e1b877136670148b65cd78d85e50d1
faisal-fiqri <mfaisalf2001@gmail.com>
2023-06-14 09:04:21 +0700

    membuat perubahan pada template halaman keranjang

--- Frontend/src/scripts/views/components/keranjang-template.js
+++ Frontend/src/scripts/views/components/keranjang-template.js
@@ -1,21 +1,4 @@
 const createKeranjangTemplate = () => `
-<div class="card">
-<div class="row">
-    <div class="col-md-8 cart">
-        <div class="title">
-            <div class="row">
-                <div class="col"><h4><b>KERANJANGKU</b></h4></div>
-            </div>
-        </div> 
-        <div class="row main align-items-center">
-        <div class="col-2"> ITEM</div>
-        <div class="col">
-            <div class="row"></div>
-        </div>
-        <div class="col">HARGA</div>
-        <div class="col">QUANTITY</div>
-        <div class="col">TOTAL HARGA</div>
-    </div> 
         <div class="row border-top border-bottom">
             <div class="row main align-items-center">
                 <div class="col-2"><img class="img-fluid" src=".." alt="gambar produk"></div>
@@ -62,24 +45,7 @@ const createKeranjangTemplate = () => `
             </div>
         </div>
     </div>
-    <div class="col-md-4 summary">
-        <hr>
-        <div class="row">
-            <div class="col" style="padding-left:0;">Total</div>
-            <div class="col text-right">RP.HARGA</div>
-        </div>
-        <div class="row">
-            <div class="col" style="padding-left:0;">Ongkos Krim</div>
-            <div class="col text-right">Free</div>
-        </div>
-        <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
-            <div class="col">Total Harga</div>
-            <div class="col text-right">RP.HARGA</div>
-        </div>
-        <button class="btn text-center">CHECKOUT</button>
-    </div>
 </div>
-
 </div>
 
 `
--- Frontend/src/scripts/views/pages/keranjang-page.js
+++ Frontend/src/scripts/views/pages/keranjang-page.js
@@ -3,9 +3,42 @@ import  { createKeranjangTemplate }  from "../components/keranjang-template";
 const KeranjangPage = {
     async render() {
       return `
-      <div class="cart-page">
-        <div id ="cart-content"> </div>
-      </div>
+      <div class="card">
+      <div class="row">
+       <div class="col-md-8 cart">
+        <div class="title">
+            <div class="row">
+                <div class="col"><h4><b>KERANJANGKU</b></h4></div>
+            </div>
+        </div> 
+        <div class="row main align-items-center">
+        <div class="col-2"> ITEM</div>
+        <div class="col">
+            <div class="row"></div>
+        </div>
+        <div class="col">HARGA</div>
+        <div class="col">QUANTITY</div>
+        <div class="col">TOTAL HARGA</div>
+            <div id=cart-content></div>
+        </div> 
+    </div>
+    <div class="col-md-4 summary">
+    <hr>
+    <div class="row">
+        <div class="col" style="padding-left:0;">Total</div>
+        <div class="col text-right">RP.HARGA</div>
+    </div>
+    <div class="row">
+        <div class="col" style="padding-left:0;">Ongkos Krim</div>
+        <div class="col text-right">Free</div>
+    </div>
+    <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
+        <div class="col">Total Harga</div>
+        <div class="col text-right">RP.HARGA</div>
+    </div>
+    <button class="btn text-center">CHECKOUT</button>
+    </div>
+    </div>
       `;
     },
    
